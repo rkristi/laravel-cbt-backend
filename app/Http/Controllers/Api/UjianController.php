d033bf552fd0835d5c7e9eb79df266b35e162b49
Rudi Kristiawan <rkristi88@gmail.com>
2023-12-27 20:39:35 +0700

    nilai_ujian

--- app/Http/Controllers/Api/UjianController.php
+++ app/Http/Controllers/Api/UjianController.php
@@ -132,4 +132,35 @@ class UjianController extends Controller
             'jawaban' => $ujianSoalList->kebenaran,
         ]);
     }
+
+    public function hitungNilaiUjianByKategori(Request $request)
+    {
+        $kategori = $request->kategori;
+        $ujian = Ujian::where('user_id', $request->user()->id)->first();
+        $ujianSoalList = UjianSoalList::where('ujian_id', $ujian->id)->get();
+
+        $ujianSoalList = $ujianSoalList->filter(function ($value, $key) use ($kategori) {
+            return $value->soal->kategori == $kategori;
+        });
+
+        $totalBenar = $ujianSoalList->where('kebenaran', true)->count();
+        $totalSoal = $ujianSoalList->count();
+        $nilai = ($totalBenar / $totalSoal) * 100;
+
+        $kategori_field = 'nilai_verbal';
+        if ($kategori == 'Numeric') {
+            $kategori_field = 'nilai_angka';
+        } else if ($kategori == 'Logika') {
+            $kategori_field = 'nilai_logika';
+        }
+
+        $ujian->update([
+            $kategori_field => $nilai
+        ]);
+
+        return response()->json([
+            'message' => 'Berhasil mendapatkan nilai',
+            'nilai' => $nilai,
+        ]);
+    }
 }
